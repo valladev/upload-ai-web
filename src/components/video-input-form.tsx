c3b686dbdceb3685b49450a3f18f6545f8fa49bc
joaobarboza <joaolucasbarbozaa@gmail.com>
2023-10-12 10:46:46 -0300

    add previewImage video

---
+++ src/components/video-input-form.tsx
@@ -0,0 +1,67 @@
+import { Separator } from "@radix-ui/react-separator";
+import { FileVideo, Upload } from "lucide-react";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { ChangeEvent, useMemo, useState } from "react";
+
+export function VideoInputForm() {
+    const [videoFile, setVideoFile] = useState<File | null>(null)
+
+    function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+        const { files } = event.currentTarget
+
+        if (!files) {
+            return
+        }
+
+        const selectedFile = files[0]
+
+        setVideoFile(selectedFile)
+    }
+
+    const previewURL = useMemo(() => {
+        if (!videoFile) {
+            return null
+        }
+
+        return URL.createObjectURL(videoFile)
+
+    }, [videoFile])
+
+    return (
+        <form className='space-y-6'>
+            <label
+                htmlFor="video"
+                className='relative border w-full flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5'
+            >
+                {previewURL ? (
+                    <video src={previewURL} controls={false} className="pointer-events-none absolute inset-0" />
+                ) : (
+                    <>
+                        <FileVideo className='w-6 h-6' />
+                        Selecione um video
+                    </>
+                )}
+            </label>
+
+            <input type="file" id='video' accept='video/mp4' className='sr-only' onChange={handleFileSelected} />
+
+            <Separator />
+
+            <div className='space-y-2'>
+                <Label htmlFor='transcription_prompt'>
+                    Prompt de transcrição
+                </Label>
+                <Textarea id='transcription_prompt' className='h-20 leading-relaxed resize-none'
+                    placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)'
+                />
+            </div>
+
+            <Button type='submit' className='w-full'>
+                Carregar video
+                <Upload className='w-4 h-4 ml-2' />
+            </Button>
+        </form>
+    )
+}
